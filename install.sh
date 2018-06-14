2f4e32dbbb094d91a4e97678e55894bc4c03c6a8
Heather Schovanec <hschovanec@usgs.gov>
2018-06-14 15:27:02 -0600

    Add basic infrastructure

---
+++ install.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+unamestr=`uname`
+env_file=environment.yml
+if [ "$unamestr" == 'Linux' ]; then
+    prof=~/.bashrc
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    matplotlibdir=~/.config/matplotlib
+elif [ "$unamestr" == 'FreeBSD' ] || [ "$unamestr" == 'Darwin' ]; then
+    prof=~/.bash_profile
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    matplotlibdir=~/.matplotlib
+else
+    echo "Unsupported environment. Exiting."
+    exit
+fi
+
+source $prof
+
+echo "Path:"
+echo $PATH
+
+VENV=strongmotion-database
+
+# Is the reset flag set?
+reset=0
+while getopts r FLAG; do
+  case $FLAG in
+    r)
+        reset=1
+
+      ;;
+  esac
+done
+
+
+# create a matplotlibrc file with the non-interactive backend "Agg" in it.
+if [ ! -d "$matplotlibdir" ]; then
+    mkdir -p $matplotlibdir
+fi
+matplotlibrc=$matplotlibdir/matplotlibrc
+if [ ! -e "$matplotlibrc" ]; then
+    echo "backend : Agg" > "$matplotlibrc"
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+elif grep -Fxq "backend : Agg" $matplotlibrc ; then
+    :
+elif [ ! grep -Fxq "backend" $matplotlibrc ]; then
+    echo "backend : Agg" >> $matplotlibrc
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+else
+    sed -i '' 's/backend.*/backend : Agg/' $matplotlibrc
+    echo "###############"
+    echo "NOTE: $matplotlibrc has been changed to set 'backend : Agg'"
+    echo "###############"
+fi
+
+
+# Is conda installed?
+conda --version
+if [ $? -ne 0 ]; then
+    echo "No conda detected, installing miniconda..."
+
+    curl $mini_conda_url -o miniconda.sh;
+    echo "Install directory: $HOME/miniconda"
+
+    bash miniconda.sh -f -b -p $HOME/miniconda
+
+    # Need this to get conda into path
+    . $HOME/miniconda/etc/profile.d/conda.sh
+
+    # remove the shell script
+    rm miniconda.sh
+else
+    echo "conda detected, installing $VENV environment..."
+fi
+
+echo "PATH:"
+echo $PATH
+echo ""
+
+
+# Choose an environment file based on platform
+echo ". $HOME/miniconda/etc/profile.d/conda.sh" >> $prof
+
+# If the user has specified the -r (reset) flag, then create an
+# environment based on only the named dependencies, without
+# any versions of packages specified.
+if [ $reset == 1 ]; then
+    echo "Ignoring platform, letting conda sort out dependencies..."
+    env_file=environment.yml
+fi
+
+# Start in conda base environment
+echo "Activate base virtual environment"
+conda activate base
+
+# Create a conda virtual environment
+echo "Creating the $VENV virtual environment:"
+conda env create -f $env_file --force
+
+# Bail out at this point if the conda create command fails.
+# Clean up zip files we've downloaded
+if [ $? -ne 0 ]; then
+    echo "Failed to create conda environment.  Resolve any conflicts, then try again."
+    exit
+fi
+
+
+# Activate the new environment
+echo "Activating the $VENV virtual environment"
+conda activate $VENV
+
+# This package
+echo "Installing strongmotion-database..."
+pip install -e .
+
+# Install default profile
+#python bin/sm_profile -c default -a
+
+# Tell the user they have to activate this environment
+echo "Type 'conda activate $VENV' to use this new virtual environment."
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='strongmotion-database',
+      version='0.1dev',
+      description='USGS ShakeMap Strong Motion Database Tools',
+      author='Mike Hearne',
+      author_email='mhearne@usgs.gov',
+      url='https://github.com/usgs/strongmotion-database',
+      packages=[],
+      scripts=['bin/getpgm'],
+      )
