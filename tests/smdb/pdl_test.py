5f15bd39aeeb1d6aac05e5627d51c7f96f1b4047
hschovanec-usgs <hschovanec@usgs.gov>
2018-07-11 14:12:35 -0600

    Add config and constants

--- tests/smdb/pdl_test.py
+++ tests/smdb/pdl_test.py
@@ -5,6 +5,8 @@ import os.path
 from datetime import datetime
 
 # local imports
+from smdb.config import get_config
+from smdb.constants import DEFAULT_CONFIG
 from smdb.pdl import store_params, get_params
 
 # third party imports
@@ -13,24 +15,10 @@ import yaml
 PARAM_DICT = {'channels': {
     'H1': {'starttime': datetime.utcnow().strftime('%Y%m%d%H%M%S%f')}}}
 
-# config file
-CONFIG_FILE = '.smdb/config.yml'
-
-
-def get_config():
-    # TODO - nuke this method once a global one exists
-    cfg_file = os.path.join(os.path.expanduser('~'), CONFIG_FILE)
-    if not os.path.isfile(cfg_file):
-        return None
-
-    config = yaml.load(open(cfg_file))
-
-    return config
-
 
 def test_put_params():
     config = get_config()
-    if config is None:
+    if config is None or config == DEFAULT_CONFIG:
         return True
     if 'pdl' not in config:
         print('This system is not configured with a PDL client. Stopping.')
@@ -40,7 +28,7 @@ def test_put_params():
 
 def test_get_params():
     config = get_config()
-    if config is None:
+    if config is None or config == DEFAULT_CONFIG:
         return True
     if 'comcat' not in config:
         fmt = 'No configuration to retrieve data from comcat. Stopping.'
